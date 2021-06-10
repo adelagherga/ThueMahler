7f688e6772f848e1f2df4639f3308b8e2b14338a
Adela Gherga <adelagherga@gmail.com>
2021-06-10 17:37:52 +0100

    Fixed minor assertion error

--- Code/GenerateSUnitEquations/RegenerateThueEqToSolve_NoSUnitEqNeeded.m
+++ Code/GenerateSUnitEquations/RegenerateThueEqToSolve_NoSUnitEqNeeded.m
@@ -1161,7 +1161,6 @@ end if;
 
 // export Thue data into .csv file
 if (ThueToSolve ne []) then
-    assert #ThueToSolve le NoThueEq;
     strRHS:= SeqEnumToString(Sort(ThueToSolve));
     fprintf ThueEqToSolve, hash cat "," cat strRHS cat "\n";
 end if;
