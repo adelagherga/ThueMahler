74266a474e6a40759c3829a3a251e37768bcfa36
Adela Gherga <adelagherga@gmail.com>
2021-07-19 11:58:48 -0700

    Generated algorithm to run Hambrook code

--- Code/Tests/PIRLtest.m
+++ Code/Tests/PIRLtest.m
@@ -7,9 +7,9 @@ Copyright © 2021, Adela Gherga, all rights reserved.
 Created: 19 July 2021
 
 Description: This program runs "tmpackAllCasesChanges1001.m" on various sets in
-	     "ObsRegeneratedTMFormData.csv". The output is used to ensure that the PIRL
-	     applied to a-values is correct. All output is printed in "PIRLtestout.txt" and
-	     "PIRLtestlog.txt".
+	     "ObsRegeneratedTMFormData.csv". The output will be used to ensure that the PIRL
+	     applied to a-values is correct and that no solutions are missing. All output is
+	     printed in "PIRLtestout.txt" and "PIRLtestlog.txt".
 
 Commentary:  Run with nohup magma /home/adela/ThueMahler/Code/Tests/PIRLtest.m &
 
