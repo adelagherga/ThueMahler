24b96ec94612bac6b1f29ec8e1d14f9e0a992d4e
Adela Gherga <adelagherga@gmail.com>
2022-08-25 01:29:16 -0400

    Minor change to output format

--- Code/EllipticCurves/computeEllipticCurves.sh
+++ Code/EllipticCurves/computeEllipticCurves.sh
@@ -11,26 +11,25 @@
 
 # Run Thue code in parallel.
 # That is, for each line "set" of Data/Forms/ThueTestForms.csv, run
-# nohup magma set:="set" Code/computeEllipticCurvesThue.m &.
-# The following code runs these jobs in the background using GNU parallel,
-# running no more than 20 jobs at once (-j20), and storing GNU parallel's
-# progress in the logfile Data/ThueTest1Log (--joblog Data/ThueTest1Log).
-nohup cat Data/Forms/ThueTestForms.csv | parallel -j20 --joblog Data/ThueTest1Log magma set:={} Code/computeEllipticCurvesThue.m 2>&1 &
+# magma set:="set" Code/computeEllipticCurvesThue.m &.
+# The following code runs these jobs using GNU parallel, running no more than
+# 20 jobs at once (-j20), and storing GNU parallel's progress in the logfile
+# Data/ThueTest1Log (--joblog Data/ThueTest1Log).
+cat Data/Forms/ThueTestForms.csv | parallel -j20 --joblog Data/ThueTest1Log magma set:={} Code/computeEllipticCurvesThue.m 2>&1
 
 # Run ThueMahler code in parallel.
 # That is, for each line "set" of Data/Forms/TMTestForms.csv, run
-# nohup magma set:="set" Code/computeEllipticCurvesTM.m &.
-# The following code runs these jobs in the background using GNU parallel,
-# running no more than 20 (-j20) jobs at once, and storing GNU parallel's
-# progress in the logfile Data/TMTest1Log (--joblog Data/TMTest1Log).
-nohup cat Data/Forms/TMTestForms.csv | parallel -j20 --joblog Data/TMTest1Log magma set:={} Code/computeEllipticCurvesTM.m 2>&1 &
+# magma set:="set" Code/computeEllipticCurvesTM.m &.
+# The following code runs these jobs using GNU parallel, running no more than
+# 20 (-j20) jobs at once, and storing GNU parallel's progress in the logfile
+# Data/TMTest1Log (--joblog Data/TMTest1Log).
+cat Data/Forms/TMTestForms.csv | parallel -j20 --joblog Data/TMTest1Log magma set:={} Code/computeEllipticCurvesTM.m 2>&1
 
 # Generate files for each conductor and populate each file with the
 # corresponding elliptic curves.
 END=500999
 START=500000
 for ((N=START;N<=END;N++)); do
-    echo $N
     touch Data/EllipticCurves/$N.csv
 done
 
