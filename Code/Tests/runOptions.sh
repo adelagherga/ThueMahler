a5e87b0280b3582ea00cfc208187c65de917f80d
Adela Gherga <adelagherga@gmail.com>
2022-10-11 11:45:23 -0400

    Testing new functions

--- Code/Tests/runOptions.sh
+++ Code/Tests/runOptions.sh
@@ -2,34 +2,81 @@
 # $ chmod u+x Code/computeEllipticCurves.sh
 
 
-while getopts ":l:" opt; do
-    case $opt in
-	l)
-	    # List of conductors.
-	    list+=("$OPTARG")
-	    ;;
-	\?)
-	    echo "Invalid option: -$OPTARG." >&2
-	    exit 1 ;;
-	:)
-	    echo "Option -$OPTARG requires an argument." >&2
-	    exit 1 ;;
-    esac
-done
-if [ -z "${list}" ]; then
-    if [ $# -eq 0 ]; then
-	echo "Argument required." >&2
-	exit 1
-    fi
-    if [ $# -eq 1 ]; then
-	list=($1)
-    elif [ $# -eq '2' ]; then
-	list=($(seq $1 $2))
+getConductorList() {
+
+    # Parses terminal input and generates the list of conductors to be resolved,
+    # along with an appropriate directory name. This function handles a single
+    # conductor N, a range of conductors [N1,...,Nn], or, with the flag -l, an
+    # arbitrary finite list of conductors.
+
+    # Parameters
+    #     N1
+    #         A single conductor.
+    #     N1 Nn: [OPTIONAL]
+    #         Two conductor values to generate the range [N1,...,Nn].
+    #     -l Ni: [OPTIONAL]
+    #         A single conductor Ni, to be parsed with the flag -l, indicating a
+    #         list. This command can be repeated to parse any finite list of
+    #         conductors.
+    # Returns
+    #
+
+    local OPTIND
+    local Nlist
+    while getopts ":l:j:" opt; do
+	case $opt in
+	    l)
+		# List of conductors.
+		list+=("${OPTARG}")
+		name+="${OPTARG}"
+		name="["${list//${IFS:0:1}/,}"]"
+		;;
+	    j)
+		jobs="${OPTARG}"
+		;;
+	    \?)
+		echo "Invalid option: -${OPTARG}." >&2
+		exit 1 ;;
+	    :)
+		echo "Option -$OPTARG requires an argument." >&2
+		exit 1 ;;
+	esac
+    done
+
+    if [ -z "${list}" ]; then
+	if [ $# -eq 0 ]; then
+	    echo "Argument required." >&2
+	    exit 1
+	fi
+	if [ $# -eq 1 ]; then
+	    list=($1)
+	    name="[$1]"
+	elif [ $# -eq '2' ]; then
+	    list=($(seq $1 $2))
+	    name="[$1..$2]"
+	else
+	    echo "Invalid argument."
+	    exit 1
+	fi
     else
-	echo "Invalid argument." >&2
-	exit 1
+	printf -v Nlist '%s,' "${list[@]}"
+	name="[""${Nlist%,}""]"
     fi
-fi
-for N in "${list[@]}"; do
-    echo "Data/EllipticCurves/${N}.csv"
-done
+    if [-z "${jobs}" ]; then
+	jobs=20
+    fi
+}
+
+main () {
+
+    # Establishes run order.
+
+    local N
+    local line
+    getConductorList
+    printf "Generating all required cubic forms for conductors in ${name}..."
+    echo ${name}
+    echo $list[@]
+}
+
+main "$@"
