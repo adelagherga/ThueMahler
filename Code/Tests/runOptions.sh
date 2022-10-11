bc8d3535d2336b42c6c3ff6ce8e5a50712682107
Adela Gherga <adelagherga@gmail.com>
2022-10-11 17:42:30 -0400

    Fixed usage printout

--- Code/Tests/runOptions.sh
+++ Code/Tests/runOptions.sh
@@ -8,11 +8,11 @@ function usage {
     echo "usage: "
     echo "  $0 N1 [N2]"
     echo "  $0 [-l N1] N2..."
-    echo "     Generate elliptic curves having conductors in the range N1 to"\
-	 "N2."
-    echo "     If N2 is omitted, generate elliptic curves having conductor N1."
     echo ""
-    echo "     -l    Generate elliptic curves having conductors in the list"\
+    echo "Generate elliptic curves having conductors in the range N1 to N2."
+    echo "If N2 is omitted, generate elliptic curves having conductor N1."
+    echo ""
+    echo "-l  Generate elliptic curves having conductors in the list"\
 	 "[N1,N2,...]."
     1>&2
     exit 1
